4b632d218442f338f18e3a119de4510b96c38a4f
viedell <49792489+viedell@users.noreply.github.com>
2025-03-09 23:33:46 +0700

    task1.py

--- task1.py
+++ task1.py
@@ -3,13 +3,3 @@ if year % 4 == 0 and (year % 100 != 0 or year % 400 == 0):
     print("Leap Year")
 else:
     print("Not a Leap Year")
-
-print("-" * 30)
-
-num = int(input("Enter a number: "))
-if num > 0:
-    print("Positive")
-elif num < 0:
-    print("Negative")
-else:
-    print("Zero")
