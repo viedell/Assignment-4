44da1d1d5c07928b2664b99b92c72e13b7a60110
viedell <49792489+viedell@users.noreply.github.com>
2025-03-09 23:39:29 +0700

    task4.py

--- task4.py
+++ task4.py
@@ -6,7 +6,7 @@ average = (math + science + english) / 3
 below_70 = sum(1 for score in [math, science, english] if score < 70)
 perfect = any(score == 100 for score in [math, science, english])
 
-if average > 75 and below_70 <= 1 or perfect:
+if average > 75 or below_70 <= 1 or perfect:
     print("Pass")
 else:
     print("Fail")
